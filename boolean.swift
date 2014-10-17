a434aa27845f5838ee7c948a1f5cccce07bdac1a
Raziye AYDIN <raziye_aydin@hotmail.com>
2014-10-17 16:28:04 +0300

    mantıksal ifadeler

---
+++ boolean.swift
@@ -0,0 +1,10 @@
+let insan_iyi=true
+let cicek_kotu=false
+
+if cicek_kotu{
+  println("çiçekler kötüdür")
+}else{
+  println("çiçekler güzeldir")
+}
+// cicek_kotu değişkeninin değeri false olduğundan else'e girecek
+// ekran çıktısı "çiçekler güzeldir"
